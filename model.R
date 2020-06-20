05edc6b62178c3adab35f6f2254a96573eb0d9cc
dmitriykuptsov <dmitriy@1000melochey.uz>
2020-06-20 18:13:09 +0500

    Added description of Holt-Winter's method

--- model.R
+++ model.R
@@ -1,6 +1,27 @@
 library(forecast)
 library(fma)
 
+#Holt (1957) and Winters (1960) extended Holt’s method to capture seasonality. 
+#The Holt-Winters seasonal method comprises the forecast equation and three 
+#smoothing equations — one for the level ℓt, one for the trend bt, and one 
+#for the seasonal component st, with corresponding smoothing parameters 
+#α, β∗ and γ. We use m to denote the frequency of the seasonality, i.e., 
+#the number of seasons in a year. For example, for quarterly data m=4, and 
+#for monthly data m=12.
+
+#There are two variations to this method that differ in the nature of the 
+#seasonal component. The additive method is preferred when the seasonal 
+#variations are roughly constant through the series, while the multiplicative 
+#method is preferred when the seasonal variations are changing proportional 
+#to the level of the series. With the additive method, the seasonal component 
+#is expressed in absolute terms in the scale of the observed series, and in 
+#the level equation the series is seasonally adjusted by subtracting the seasonal 
+#component. Within each year, the seasonal component will add up to approximately 
+#zero. With the multiplicative method, the seasonal component is expressed in 
+#relative terms (percentages), and the series is seasonally adjusted by dividing 
+#through by the seasonal component. Within each year, the seasonal component 
+#will sum up to approximately m.
+
 # Load the data
 yandex<-scan("yandex.csv");
 google<-scan("google.csv");
@@ -32,11 +53,11 @@ abline(fit, col="dark blue", lwd=6);
 # Print model parameters
 print(fit);
 
-# Predict using Holt-Winters model
+# Predict using Holt-Winters' model
 p <- predict(hw, 52, prediction.interval = FALSE);
 
 # Plot predicted data
-points(seq(283, 334), p, col="light blue", lwd=6, type="l")
+points(seq(283, 334), p, col="light blue", lwd=6, type="l");
 
 # Plot linear regression
 abline(fit, col="dark blue", lwd=6);
@@ -60,4 +81,8 @@ grid(col="black");
 legend("topleft", c("Real data (Google)", "Linear regression", "Holt-Winters prediction"), col=c("dark red", "dark blue", "light blue"), bty="n", lwd=6);
 
 # Write plots to the file
+dev.off();
+
+pdf("yandex_acf.pdf");
+acf(yandex, h=282);
 dev.off();
