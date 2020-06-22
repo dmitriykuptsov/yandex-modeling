87a09926993dd960023f5afa99ebf46033517c45
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2020-06-22 17:27:11 +0500

    Added autocorrelation

--- autocorrelation.R
+++ autocorrelation.R
@@ -5,7 +5,10 @@ model<-lm(formula = visits ~ weeks, data = yandex);
 #level<-668358233+1798283*yandex$weeks;
 levels<-predict(model, newdata=data.frame(weeks=seq(1, 282)));
 pdf("autocorrelation.pdf");
-acf(yandex$visits-levels, lag=282);
+# Just as correlation measures the extent of a linear relationship 
+# between two variables, autocorrelation measures the linear relationship 
+# between lagged values of a time series.
+acf(yandex$visits-levels, lag.max=282);
 grid(col="black");
 dev.off();
 
